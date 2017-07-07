fa44a06842c63245ef0318cfc6f7eb8f3cb2dd35
luokexidelei <zhangying2929@gmail.com>
2017-07-07 16:15:07 +0800

    程序员的三行情书

---
+++ nodedemo2/nothing.js
@@ -0,0 +1,70 @@
+//自从看到你之后就爱上了单纯的你
+long long love = 0;
+
+//时光在增加，爱你的心也在升温
+for(long long time = 1; time < love; ++love; ++time)
+
+    //如果你不爱我，我将会等到时间的尽头，独自心碎
+    if(love < 0) break;
+
+
+//寻遍整个世界，只为找到你
+foreach(var you in worlds) {
+
+    //当我发现你是我的真爱时
+    if(you == "myLove")
+
+        //那么你就是我要找寻的人，my wife
+        myHeart["myWife"] = you;
+}
+
+
+//生活中总是出现这样那样的意外
+try{living();}
+
+//只要我们携手共同面对
+catch (Exception e) {faceTogether();}
+
+//最终只会让我们的爱更深
+finally{ours.love++;}
+
+
+
+//定义一个变量：我们的相爱时间
+long lovetime;
+
+//如果我们之间是真爱
+if(love == true)
+
+    //那么从我们相爱的那一秒起，我们注定将无限循环，执子之手，与子偕老
+    for(lovetime = 1; lovetime > 1; lovetime++)
+
+
+//好的爱情就是你确定你们在一起，有一个可以预见的未来
+char TrueLove[] = "Bright future";
+
+//真正的爱人是可以为你引路，让你越来越明确自己的方向，变成一个前途光明的、越来越优秀的人
+char *p  = TrueLove;
+
+//最伟大的爱人不是讨好和给予，而是为你指引前进的方向
+printf("A great Lover is one who will lead you to a s%",TrueLove);
+
+/** 只要我的人生不终止，你一直是我的小甜心*/
+char *MySweetHeart = You;
+
+/** 自从那天与你邂逅，我愿往后生命里的每一天都有你的烙印，至死方休*/
+for(int i = Mylife.AnEncounterWithYou; HeatBeat -> next != Stop;)
+
+    /** 若你不爱，默默守护，不打扰我的温柔；若你倾心，爱你是我一生荣光*/
+    (YourLover != Me) ? SlientGuard(You) : DeepLove(You)
+
+
+
+/** 如果冬天里你的手是冰冷的 */
+if(you.hand == cold && weather == winter)
+
+    /** 将我手上的温度全部给你 **/
+    giveYouMyLove(myHand.temprature, yourHand.temprature);
+
+    /***换取你幸福的脸庞 */
+    return yourHappyFace;
